8dcab97bab431224ea45cd4c1747313525f9b69d
matthewlabenznws <matthew.labenz@noaa.gov>
2026-08-23 13:25:51 -0500

    Update hrrr_ecape_regional.py

--- hrrr_ecape_regional.py
+++ hrrr_ecape_regional.py
@@ -1818,7 +1818,7 @@ ECAPE_SMOOTH_SIGMA = 0.65
 # Barbs are plotted from the sampled ECAPE grid.
 # ------------------------------------------------------------
 
-BARB_SKIP = 20
+BARB_SKIP = 16
 # ------------------------------------------------------------
 # MEAN LCL-LFC RELATIVE HUMIDITY CONTOURS
 #
