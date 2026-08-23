99b25d0d4473f13aa55b26a60ca048e2505319f2
matthewlabenznws <matthew.labenz@noaa.gov>
2026-08-23 18:15:20 -0500

    Update hrrr_ecape_regional.py

--- hrrr_ecape_regional.py
+++ hrrr_ecape_regional.py
@@ -4516,7 +4516,7 @@ def process_forecast_hour(
             inline=True,
             inline_spacing=6,
             fmt=lambda value: f"{int(value)}%",
-            fontsize=6
+            fontsize=7
         )
 
         for label in rh_labels:
@@ -4528,7 +4528,7 @@ def process_forecast_hour(
             label.set_path_effects(
                 [
                     pe.withStroke(
-                        linewidth=1.0,
+                        linewidth=2.0,
                         foreground="white"
                     )
                 ]
